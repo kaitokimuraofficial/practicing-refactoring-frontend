ce0fea5123ce3e39d19906afc923db907078291b
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-01-12 00:18:51 +0900

    finished section 6

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react',
+      '@babel/preset-typescript',
+      '@babel/preset-flow'
+    ],
+    plugins: [
+      '@babel/plugin-transform-runtime',
+      'babel-plugin-styled-components'
+    ]
+  }
